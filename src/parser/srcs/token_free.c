e7eea3fc82139bdf5b3111c230db9c585e2dbff7
lalouabd <lalouabd123@gmail.com>
2021-02-11 17:00:27 +0100

    yLLO

---
+++ src/parser/srcs/token_free.c
@@ -0,0 +1,14 @@
+#include "cub3d.h"
+
+void free_token(void *item)
+{
+    t_token *this = (t_token *) item;
+
+    if (this != NULL)
+    {
+        if (this->token != NULL)
+            free(this->token);
+        if (this->values.arr != NULL)
+            this->values.free(&this->values, &free);
+    }
+}
